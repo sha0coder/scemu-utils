d6f2aa1cee9a4e7b467c712b235cc8c94d266d76
sha0 <sha0@saturn>
2023-02-22 09:35:36 +0100

    scemu test scripts moved here

---
+++ run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [[ "$OSTYPE" == "darwin21" ]]
+then
+  TARGET="x86_64-apple-darwin"
+elif [[ "$OSTYPE" == "linux-gnu" ]]
+then
+  TARGET="x86_64-unknown-linux-gnu"
+elif [[ "$OSTYPE" == "linux-musl" ]]
+then
+  TARGET="x86_64-unknown-linux-musl"
+elif [[ "$OSTYPE" == "msys" ]]
+then
+  TARGET="x86_64-pc-windows-msvc"
+else
+  echo "unknown OSTYPE: $OSTYPE"
+  exit
+fi
+
+export RUST_BACKTRACE=1
+
+cargo run \
+  --target $TARGET \
+  --release \
+  -- \
+  -6 \
+  -vvv \
+  --regs \
+  --memory \
+  -f $1 > ./scripts/scemu-output.txt
+node ./scripts/scemu-vs-x64dbg-parser.js
